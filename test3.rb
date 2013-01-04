154bcc3956e54deeb1454f8d1ce9f29c78dfeac4
Zhiyiw <privaterbok@gmail.com>
2013-01-03 16:14:12 -0800

    modify test 3

--- test3.rb
+++ test3.rb
@@ -2,10 +2,15 @@ def fib_up_to(max)
 i1, i2 = 1, 1 
 while i1 <= max
 yield i1
+puts "i1= #{i1}"
+puts "i2= #{i2}"
     i1, i2 = i2, i1+i2
+
 end 
 end
 
 
 fib_up_to(1000) {|f| print f, " " } 
+#fib_up_to(1000) {|(f-1)| print f, " " }  this won't work
+#fib_up_to(1000) {|f| print (f+1), " " }  this will work
 puts
